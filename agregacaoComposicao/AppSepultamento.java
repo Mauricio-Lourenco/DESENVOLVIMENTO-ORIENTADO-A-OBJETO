22a3708970cfcb87ea66af61906bfce4305be2fe
Mauricio Lourenco <junior_301@live.com>
2023-01-28 17:33:20 -0300

    Update AppSepultamento.java
    teste commit

--- agregacaoComposicao/AppSepultamento.java
+++ agregacaoComposicao/AppSepultamento.java
@@ -12,6 +12,8 @@ public class AppSepultamento {
 		System.out.println("Situação Inicial: " + corpo.getSituacao());
 		System.out.println("================================================");
 
+		
+
 		sepultamento.sepultar();
 		sepultamento.getTumulo().isOcupado();
 
